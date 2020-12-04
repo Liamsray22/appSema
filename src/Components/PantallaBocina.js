d930c37ba95b12fd23f5447c96a212f42ffddfd3
Liamsray22 <eliamsolis2201@gmail.com>
2020-12-03 23:23:09 -0400

    pantalla con bocina casi ready

--- src/Components/PantallaBocina.js
+++ src/Components/PantallaBocina.js
@@ -1,12 +1,35 @@
-import React, { useState} from 'react'
+import React, { useState ,useEffect} from 'react'
 import Menu from './Menu'
 import sema1 from '../images/SemaforoLuzRoja.png'
 import sema2 from '../images/semaforoLuzVerde.png'
 import '../Css/bocina.css'
+import axios from 'axios'
 
 const PantallaBocina = ()=>{
 
-    const[cruzar, setCruzar] = useState(true)
+    var num = 3
+    useEffect(()=>{
+        setInterval(()=>{
+            axios.get('http://localhost:3001/Bocinas').then(
+    function (response) {
+    console.log(response.data)
+    if(response.data == "verde"){
+        setCruzar(true)
+        speechSynthesis.speak(new SpeechSynthesisUtterance(num))
+    }else{
+        setCruzar(false)
+
+    }
+  }).catch(
+    function (error) {
+    console.log(error);
+  });
+        },3000)
+        
+    })
+    
+
+    const[cruzar, setCruzar] = useState(false)
     return(
         <div>
             <Menu/>
