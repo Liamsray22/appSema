98416469e85951a3602faa97198f1bfccdff2b21
Liamsray22 <62081292+Liamsray22@users.noreply.github.com>
2020-12-04 12:14:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import ModuloRegistro from './Pages/ModuloRegistro'
-import ModuloLogin from './Pages/ModuloLogin'
-import Historial from './Pages/Historial'
 import SideBarContainer from './Components/Containers/SideBarContainer'
-import Config from './Components/Config'
 import Button from './Components/Button'
 import PantallaBocina from './Components/PantallaBocina'
 
@@ -13,11 +9,7 @@ import PantallaBocina from './Components/PantallaBocina'
 const App = ()=>(
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={ModuloLogin} />
-            <Route exact path="/registro" component={ModuloRegistro} />
-            <Route exact path="/principal" component={SideBarContainer} />
-            <Route exact path="/historial" component={Historial} />
-            <Route exact path="/config" component={Config} />
+            <Route exact path="/" component={SideBarContainer} />
             <Route exact path="/funcion" component={Button} />
             <Route exact path="/pbocina" component={PantallaBocina} />
         </Switch>
