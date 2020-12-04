5c417b8c2fbc0923a440a799460b4e077d654659
Liamsray22 <62081292+Liamsray22@users.noreply.github.com>
2020-12-04 12:19:48 -0400

    Update SideBarContainer.js

--- src/Components/Containers/SideBarContainer.js
+++ src/Components/Containers/SideBarContainer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Functions} from '../../Data/functions'
 import Card from '../Card'
 import Menu from '../Menu'
 
@@ -9,17 +8,7 @@ const SideBarContainer =()=>(
             <Menu/>
 
     <div  className="page">
-        {Functions.map((item, index)=>{
-            return(<Card
-                imagen = {item.img}
-                texto = {item.text}
-                clase = {item.className}
-                link = {item.link}
-            />
-            )
-        })
-    
-    }
+       Hola
     </div>
     </>
 
